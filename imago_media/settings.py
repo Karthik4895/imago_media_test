17d19119a78a952f6bfea9f31011ec1423187a33
Karthik J <karthik.official4895@gmail.com>
2025-02-17 10:37:21 +0530

    Adding Procfile

--- imago_media/settings.py
+++ imago_media/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_7nr&d7dhunvv&i1_5xdm**p+mkp+qpg7%52!c=g=p34_&@^8#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
